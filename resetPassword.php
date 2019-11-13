dcc85f9438497bf5fa625e0ad84f7ecae414e69e
unknown <kumaranuranjan652@gmail.com>
2019-11-14 01:36:14 +0530

    Merge branch 'master' of https://github.com/anuranjankr/studentGrievancePortal

---
+++ resetPassword.php
@@ -0,0 +1,67 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>IIIT Bhagalpur</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="google-site-verification" content="mYwq4kbzhLz2dI8Vg4EGJodW6puVKaFdzln7U-5aH-A" />
+
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/custom-styles.css">
+  <link rel="stylesheet" href="css/centerNav.css">
+  <link rel="stylesheet" href="css/login_demo.css">
+
+  <link rel="shortcut icon" href="img/150dpi.png" type="image/x-icon" />
+
+  <link rel = "stylesheet" href ="css/EmailCards.css">
+  <script src="js/3.4.1-jquery.min.js"></script>
+</head>
+
+<body>
+
+  <div class="header-wrapper">
+    <div class="site-name">
+      <table><tr><td><a href="iiitbh.ac.in"><img src="img/150dpi.png" height=80px></a></td><td><h1>भारतीय सूचना प्रौद्योगिकी संस्थान भागलपुर<br>Indian Institute of Information Technology Bhagalpur</h1></td></tr></table>
+    </div>
+  </div>
+  <!-- Top navigation -->
+<div class="topnav">
+  <!-- Centered link -->
+  <div class="topnav-centered">
+    <a><h1>Student Grievance Portal</h1><a>
+  </div>
+
+
+  <!-- Right-aligned links -->
+  <div class="topnav-right">
+    <a href="iiitbh.ac.in">IIITBH</a>
+  </div>
+
+</div>
+
+  <div class="form-box">
+
+    <form id="user" class="input-group">
+    <div class="head-box" style="margin-top:-50px;">
+      <label style="font-size:25px; display:inline-block;">Change Your Password</label>
+    </div>
+    <br>
+    <input type="text" name="otp" class="input-field" placeholder="OTP" id="o_t_p"required>
+    <input type="text" name="newPass" class="input-field" placeholder="New Password" id="new_pass"required>
+    <input type="text" name="cnfPass" class="input-field" placeholder="Confirm Password" id="cnf_pass"required>
+    <button type="submit" class="submit-btn">Change Password</button>
+    </form>
+
+  </div>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+
+</body>
+</html>
